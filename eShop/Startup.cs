552c3fdc4699a986709e6cf8d3b6d6768ca2702b
hgp004 <458263469@qq.com>
2018-04-18 23:32:28 +0800

    applicationServices

--- eShop/Startup.cs
+++ eShop/Startup.cs
@@ -27,6 +27,7 @@ namespace eShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //app.ApplicationServices
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
